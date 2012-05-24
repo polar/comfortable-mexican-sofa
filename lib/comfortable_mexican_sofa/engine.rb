ceaa98a66c40f094ba2fa41fb5c7102208755fdd
Polar Humenn <polar@adiron.com>
2012-05-24 15:11:43 -0400

    do not load the initializers if working as engine

--- lib/comfortable_mexican_sofa/engine.rb
+++ lib/comfortable_mexican_sofa/engine.rb
@@ -6,7 +6,11 @@ require 'mime/types'
 
 module ComfortableMexicanSofa
   class Engine < ::Rails::Engine
-    # ...
+
+    # We get rid of running the initializers as an engine
+    # as the ActiveRecord as default ORM will not start
+    # if there isn't an ActiveRecord setup.
+    config.paths["config/initializers"] = []
   end
 end
 
